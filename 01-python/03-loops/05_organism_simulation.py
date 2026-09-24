0afca3cb10c05987146f053c7bbe9216f5351eb8
Aashutosh Bairagi <aashutosh.bairagi05@gmail.com>
2026-09-24 23:15:17 +0530

    fix: streamline energy cost calculation and improve loop structure in organism simulation

--- 01-python/03-loops/05_organism_simulation.py
+++ 01-python/03-loops/05_organism_simulation.py
@@ -2,27 +2,33 @@ age = 0
 energy = 100
 alive  = True
 
+print("Age: ",age)
+print("Energy: ",energy)
+print("Alive: ",alive)  
+
+
 while alive:
-
-
+  energy_cost = 0
+  
   if 0 <= age <=18:
-    age += 1
-    energy -= 1
+    energy_cost = 1
   elif 18 < age <=30:
-    age += 1
-    energy -= 2
+    energy_cost = 2
   elif 30 < age <=60:
-    age += 1
-    energy -= 2
-    if energy <= 0:
-      break;
+    energy_cost = 3
   elif 60 < age <=100:
-    age += 1
-    energy -= 5
-    if energy <= 0:#breaks the  loop/prevents printing the loop  when organism energy reaches less than or equal to 0 internal before printing it.
-      break;
+    energy_cost = 5
+
+  if energy < energy_cost:
+    alive = False
+  else:
+    energy-=energy_cost
+    age+=1
+
   if energy == 0:
     alive = False
+
+
   
   print("Age: ",age)
   print("Energy: ",energy)
